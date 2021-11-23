75bb500be4ffbe28dc43ae73bbcc5058c411db60
Sean Morrison <brlcad@gmail.com>
2021-11-23 02:57:28 -0500

    basics called for returning the numbers as output

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
 
 int main(int ac, char *av[]) {
 
-  std::cout << "Usage: " << av[0] << " [count]" << std::endl;
+  if (ac > 2) {
+    std::cout << "Usage: " << av[0] << " [count]" << std::endl;
+    return 1;
+  }
+
+  int count = 1;
+  if (ac == 2) {
+    count = abs(atoi(av[1]));
+  }
+
+  srand(time(0));
+  while (count--) {
+    std::cout << rand() << std::endl;
+  }
 
   return 0;
 }
